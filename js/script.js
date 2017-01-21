2466a9516d3dc2ad2f0ab77a3deef1d591b2fceb
MJGC-Jonathan <jblackops99@gmail.com>
2017-01-20 17:21:46 -0700

    Theming
    Added theme switcher.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+google.load("jquery", "1.3.1");
+google.setOnLoadCallback(function()
+{
+    $(".colord").click(function(){
+        $("link").attr("href", "css/style.css");
+        return false;
+    });
+    $(".colorblue").click(function(){
+        $("link").attr("href", "css/blue.css");
+        return false;
+    });
+    
+    $(".colorgreen").click(function(){
+        $("link").attr("href", "css/green.css");
+        return false;
+    });
+    
+    $(".colororange").click(function(){
+        $("link").attr("href", "css/orange.css");
+        return false;
+    });
+
+});
